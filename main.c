733a0e7faa0196ac507ffd1426c6892cf7a4967f
rbardet <robinbt2004@gmail.com>
2025-09-30 02:02:09 +0200

    Add: ft_strdup test

--- main.c
+++ main.c
@@ -12,6 +12,7 @@ extern char *ft_strcpy(char *dest, const char *src);
 extern int ft_strcmp(const char *s1, const char *s2);
 extern ssize_t ft_write(int fd, const void *buf, size_t count);
 extern ssize_t ft_read(int fd, void *buf, size_t count);
+extern char *ft_strdup(const char *s);
 
 void test_strlen() {
 	const char *str1 = "Hello World!";
@@ -179,7 +180,7 @@ void test_read() {
 	errno = 0;
 	len = ft_read(fd, buff2, sizeof(buff2) - 1);
 	if (len >= 0) {
-		buff[len] = '\0';
+		buff2[len] = '\0';
 	}
 	printf("\nmine: in real file: \n%d %s\n", len, buff2);
 	printf("errno :%d\n", errno);
@@ -202,6 +203,40 @@ void test_read() {
 	close(fd);
 }
 
+void test_strdup() {
+	char *str1 = "Hello World!";
+
+	char *str2 = NULL;
+	errno = 0;
+	str2 = strdup(str1);
+	printf("real function: %s\n", str2);
+	printf("errno: %d\n", errno);
+	free(str2);
+
+	char *str3 = NULL;
+	errno = 0;
+	str3 = ft_strdup(str1);
+	printf("mine: %s\n", str3);
+	printf("errno: %d\n", errno);
+	free(str3);
+
+	char *str4 = "Le club est né de la fusion des deux clubs professionnels de la ville, l'Olympique lillois et le Sporting Club fivois, situés dans deux quartiers lillois différents, qui avaient participé aux sept premières éditions du championnat de France professionnel avant l'arrêt de la compétition à cause de la Seconde Guerre mondiale. D'abord appelé Stade lillois, le club prend dès novembre 1944 le nom de Lille Olympique Sporting Club, qui rappelle ceux des deux clubs d'origine. Abrégé en Lille OSC, il donne lieu à l'acronyme LOSC, qui s'impose pour former le nom courant du club, avec mention redondante du nom de la ville : LOSC Lille.";
+
+	char *str5 = NULL;
+	errno = 0;
+	str5 = ft_strdup(str4);
+	printf("real function: %s\n", str5);
+	printf("errno: %d\n", errno);
+	free(str5);
+
+	char *str6 = NULL;
+	errno = 0;
+	str6 = ft_strdup(str4);
+	printf("mine: %s\n", str6);
+	printf("errno: %d\n", errno);
+	free(str6);
+}
+
 int main(void) {
 	printf("-----ft_strlen-----\n");
 	test_strlen();
@@ -213,6 +248,8 @@ int main(void) {
 	test_write();
 	printf("-----ft_read-----\n");
 	test_read();
+	printf("-----ft_strdup-----\n");
+	test_strdup();
 	return (0);
 }
 
