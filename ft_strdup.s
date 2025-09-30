089e95b6c9300c7719d1d4e1345077147b7d1ba3
rbardet <robinbt2004@gmail.com>
2025-09-30 02:01:54 +0200

    Feat: ft_strdup

---
+++ ft_strdup.s
@@ -0,0 +1,28 @@
+extern malloc
+extern ft_strlen
+extern ft_strcpy
+extern __errno_location
+
+section .text
+	global ft_strdup
+
+ft_strdup:
+	XOR bl, bl
+	PUSH rdi
+	CALL ft_strlen
+	MOV rdi, rax
+	INC rdi
+	CALL malloc
+	CMP rax, 0
+	JL .err
+	POP rsi
+	MOV rdi, rax
+	CALL ft_strcpy
+	RET
+.err:
+	NEG rax
+	MOV rcx, rax
+	CALL __errno_location
+	MOV [rax], rcx
+	MOV rax, -1
+	RET
