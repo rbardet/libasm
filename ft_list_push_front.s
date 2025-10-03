eb3a8d925a2f36ab7c18c6864727af4aced5425d
Robin <robinbt2004@gmail.com>
2025-10-03 13:20:06 +0200

    Fix: pointers error

--- ft_list_push_front.s
+++ ft_list_push_front.s
@@ -1,12 +1,14 @@
 extern malloc
 section .text
 	global ft_list_push_front
-
 ft_list_push_front:
-	MOV rbx, [rdi]
+	PUSH rsi
+	MOV rbx, rdi
 	MOV rdi, 16
 	CALL malloc
+	POP rsi
 	MOV [rax], rsi
-	MOV [rax + 8], rbx
-	MOV [rdi], rax
+	MOV rcx, [rbx]
+	MOV [rax + 8], rcx
+	MOV [rbx], rax
 	RET
