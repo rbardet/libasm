c497ff87ea1a0584c522390da289909e677a830a
Robin <robinbt2004@gmail.com>
2025-10-03 13:20:40 +0200

    Add: ft_list_push_front test

--- main.c
+++ main.c
@@ -7,12 +7,20 @@
 #include <fcntl.h>
 #include <errno.h>
 
+typedef struct s_list
+{
+	void *data;
+	struct s_list *next;
+} 				t_list;
+
 extern size_t ft_strlen(const char *s);
 extern char *ft_strcpy(char *dest, const char *src);
 extern int ft_strcmp(const char *s1, const char *s2);
 extern ssize_t ft_write(int fd, const void *buf, size_t count);
 extern ssize_t ft_read(int fd, void *buf, size_t count);
 extern char *ft_strdup(const char *s);
+extern t_list *ft_list_push_front(t_list **begin_list, void *data);
+extern int ft_list_size(t_list *begin_list);
 
 void test_strlen() {
 	const char *str1 = "Hello World!";
@@ -47,22 +55,22 @@ void test_strcpy() {
 	const char *str2 = "Lille OSC";
 	str1_1 = malloc(sizeof(char) * ft_strlen(str2) + 1);
 
-	printf("real func: %s\n", strcpy(str1_1, str2) + 1);
-	printf("mine: %s\n", strcpy(str1_1, str2));
+	printf("real func: %s\n", strcpy(str1_1, str2));
+	printf("mine: %s\n", ft_strcpy(str1_1, str2));
 	free(str1_1);
 
 	const char *str3 = "Le club est né de la fusion des deux clubs professionnels de la ville, l'Olympique lillois et le Sporting Club fivois, situés dans deux quartiers lillois différents, qui avaient participé aux sept premières éditions du championnat de France professionnel avant l'arrêt de la compétition à cause de la Seconde Guerre mondiale. D'abord appelé Stade lillois, le club prend dès novembre 1944 le nom de Lille Olympique Sporting Club, qui rappelle ceux des deux clubs d'origine. Abrégé en Lille OSC, il donne lieu à l'acronyme LOSC, qui s'impose pour former le nom courant du club, avec mention redondante du nom de la ville : LOSC Lille.";
 	str1_1 = malloc(sizeof(char) * ft_strlen(str3) + 1);
 
 	printf("real func: %s\n", strcpy(str1_1, str3));
-	printf("mine: %s\n", strcpy(str1_1, str3));
+	printf("mine: %s\n", ft_strcpy(str1_1, str3));
 	free(str1_1);
 
 	const char *str4 = "";
 	str1_1 = malloc(sizeof(char) * ft_strlen(str4) + 1);
 
 	printf("real func: %s\n", strcpy(str1_1, str4));
-	printf("mine: %s\n", strcpy(str1_1, str4));
+	printf("mine: %s\n", ft_strcpy(str1_1, str4));
 	free(str1_1);
 }
 
@@ -237,20 +245,37 @@ void test_strdup() {
 	free(str6);
 }
 
+// int main(void) {
+// 	printf("-----ft_strlen-----\n");
+// 	test_strlen();
+// 	printf("-----ft_strcpy-----\n");
+// 	test_strcpy();
+// 	printf("-----ft_strcmp-----\n");
+// 	test_strcmp();
+// 	printf("-----ft_write-----\n");
+// 	test_write();
+// 	printf("-----ft_read-----\n");
+// 	test_read();
+// 	printf("-----ft_strdup-----\n");
+// 	test_strdup();
+// 	return (0);
+// }
+
+//for bonus
 int main(void) {
-	printf("-----ft_strlen-----\n");
-	test_strlen();
-	printf("-----ft_strcpy-----\n");
-	test_strcpy();
-	printf("-----ft_strcmp-----\n");
-	test_strcmp();
-	printf("-----ft_write-----\n");
-	test_write();
-	printf("-----ft_read-----\n");
-	test_read();
-	printf("-----ft_strdup-----\n");
-	test_strdup();
-	return (0);
+	t_list *list = NULL;
+	char *test = "LILLE";
+	char *test2 = "LOSC";
+	ft_list_push_front(&list, test);
+	ft_list_push_front(&list, test2);
+	t_list *list2 = NULL;
+	while (list) {
+		printf("%s\n", (char *)list->data);
+		list2 = list;
+		list = list->next;
+		free(list2);
+
+	}
 }
 
 // 1er argument rdi
